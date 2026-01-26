f3f995ae68c2ad3776c2958fbf259a15149e5394
je-jmo <jjm164545@gmail.com>
2026-01-26 23:03:58 +0900

    hicc/google-signup

---
+++ src/main/java/HICC_Project/OnePageMe/OnePageMeApplication.java
@@ -0,0 +1,13 @@
+package HICC_Project.OnePageMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnePageMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnePageMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/HICC_Project/OnePageMe/OnePageMeApplicationTests.java
@@ -0,0 +1,13 @@
+package HICC_Project.OnePageMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnePageMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
