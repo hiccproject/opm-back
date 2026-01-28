33e767b9e42f453f02755c1a150199a8ce0ad45c
he-on <hyeonjub2911@g.hongik.ac.kr>
2026-01-28 21:16:20 +0900

    fix: 프론트 배포 서버 추가

--- src/main/java/opm/example/opm/common/config/SwaggerConfig.java
+++ src/main/java/opm/example/opm/common/config/SwaggerConfig.java
@@ -42,7 +42,8 @@ public class SwaggerConfig {
         return new OpenAPI()
                 .info(apiInfo())
                 .servers(List.of(
-                        new Server().url("/").description("Default Server URL"), // 상대 경로 (가장 추천)
+                        new Server().url("/").description("Default Server URL"),
+                        new Server().url("https://api.onepageme.kr").description("Production Server (New)"),
                         new Server().url("http://localhost:8080").description("Local Server"),
                         new Server().url("https://onepageme.kr").description("Production Server")
                 ))
