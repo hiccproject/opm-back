3aafb78a147ece012389d41d27c8f99ddcc1ec5c
he-on <hyeonjub2911@g.hongik.ac.kr>
2026-02-11 01:18:12 +0900

    fix : 포트폴리오 둘러보기 페이지 형식으로 수정

---
+++ src/main/java/opm/example/opm/common/response/PagedResponse.java
@@ -0,0 +1,35 @@
+package opm.example.opm.common.response;
+
+import lombok.Getter;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@Getter
+public class PagedResponse<T> {
+    private List<T> content;
+    private int page;
+    private int size;
+    private long totalElements;
+    private int totalPages;
+    private boolean last;
+
+    public PagedResponse(List<T> content, int page, int size, long totalElements, int totalPages, boolean last) {
+        this.content = content;
+        this.page = page;
+        this.size = size;
+        this.totalElements = totalElements;
+        this.totalPages = totalPages;
+        this.last = last;
+    }
+
+    public static <T> PagedResponse<T> from(Page<T> page) {
+        return new PagedResponse<>(
+                page.getContent(),
+                page.getNumber(),
+                page.getSize(),
+                page.getTotalElements(),
+                page.getTotalPages(),
+                page.isLast());
+    }
+}
